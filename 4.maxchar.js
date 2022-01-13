44017e5a5637ee54a3ce53e2167a66112fe07be6
Matthew Valicoff <matthewvalicoff@Matthews-MacBook-Pro.local>
2022-01-12 19:18:04 -0600

    algo save

---
+++ 4.maxchar.js
@@ -0,0 +1,8 @@
+// Given a string, return the character that is most commonly used in a string.
+// --- Examples
+// maxChar("abccccdef") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+    
+}
